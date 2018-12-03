c78b9cfdab1b8d6210ef517b5020bf119a8104d5
Vlad Popovici <vlad.popovici@ullink.com>
2018-12-03 16:05:28 +0200

    extend environment. Define alias for TD\

--- app/Main.hs
+++ app/Main.hs
@@ -4,17 +4,20 @@ module Main where
     import Data.Either
     import Text.Read (readMaybe)
     import System.IO
+    import Data.List (find)
     
     data Square = Square Int Int deriving (Show, Eq)
-    data XO = EMPTY | X | O deriving (Eq)
+    data XO = E | X | O deriving (Eq)
     data Player = P1 | P2 deriving (Eq)
     type Row = [XO]
     type Board = [Row]
+    type Moves = [Square]
+    type States = [Board]
     
     instance Show XO where
         show X = "x"
         show O = "o"
-        show EMPTY = " "
+        show E = " "
 
     instance Show Player where
         show P1 = "Player 1"
@@ -51,7 +54,7 @@ module Main where
         return p
     
     initRow :: Row
-    initRow = [EMPTY, EMPTY, EMPTY]
+    initRow = [E, E, E]
     
     initBoard :: Board
     initBoard = [initRow, initRow, initRow]
@@ -69,11 +72,11 @@ module Main where
 
     setSquare :: Row -> Int -> Player -> (Row, Player)
     setSquare [s0, s1, s2] 0 p =
-        if s0 /= EMPTY then ([s0, s1, s2], p) else ([sign(p), s1, s2], nextPlayer(p))
+        if s0 /= E then ([s0, s1, s2], p) else ([sign(p), s1, s2], nextPlayer(p))
     setSquare [s0, s1, s2] 1 p =
-        if s1 /= EMPTY then ([s0, s1, s2], p) else ([s0, sign(p), s2], nextPlayer(p))
+        if s1 /= E then ([s0, s1, s2], p) else ([s0, sign(p), s2], nextPlayer(p))
     setSquare [s0, s1, s2] 2 p =
-        if s2 /= EMPTY then ([s0, s1, s2], p) else ([s0, s1, sign(p)], nextPlayer(p))
+        if s2 /= E then ([s0, s1, s2], p) else ([s0, s1, sign(p)], nextPlayer(p))
     setSquare r _ p = (r, p)
     
     move :: Board -> Square -> Player -> (Board, Player)
@@ -103,7 +106,7 @@ module Main where
     putSpace = putChar ' '
     
     printSquare :: XO -> IO()
-    printSquare EMPTY = putSpace
+    printSquare E = putSpace
     printSquare X = putChar 'x'
     printSquare O = putChar 'o'
         
@@ -134,22 +137,26 @@ module Main where
         printRowSep
         printRow r2
         putStrLn ""
+        putStrLn ""
     
-    isRowWin :: Board -> Bool
-    isRowWin b = any (\r -> r!!0 /= EMPTY && r!!0 == r!!1 && r!!1 == r!!2) b
+    isRowWin :: Board -> Player -> Bool
+    isRowWin b p = any (\r -> r!!0 == sign(p) && r!!0 == r!!1 && r!!1 == r!!2) b
     
-    isColWin :: Board -> Bool
-    isColWin [r0, r1, r2] = any (\col -> r0!!col /= EMPTY && r0!!col == r1!!col && r1!!col == r2!!col) [0..2]
+    isColWin :: Board -> Player -> Bool
+    isColWin [r0, r1, r2] p = any (\col -> r0!!col == sign(p) && r0!!col == r1!!col && r1!!col == r2!!col) [0..2]
 
-    isDigWin :: Board -> Bool
-    isDigWin [r0, r1, r2] = (r0!!0 /= EMPTY && r0!!0 == r1!!1 && r1!!1== r2!!2) || (r0!!2 /= EMPTY && r0!!2 == r1!!1 && r1!!1 == r2!!0)
+    isDigWin :: Board -> Player -> Bool
+    isDigWin [r0, r1, r2] p = (r0!!0 == sign(p) && r0!!0 == r1!!1 && r1!!1== r2!!2) || (r0!!2 /= E && r0!!2 == r1!!1 && r1!!1 == r2!!0)
 
-    isWin :: Board -> Bool
-    isWin b = isRowWin b || isColWin b || isDigWin b
+    isWin :: Board -> Player -> Bool
+    isWin b p = isRowWin b p || isColWin b p || isDigWin b p
+
+    notWin :: Board -> Player -> Bool
+    notWin b p = not(isWin b p)
 
     play :: Board -> Player -> IO()
     play b p = 
-        if isWin b then
+        if isWin b (nextPlayer p) then
             print(show (nextPlayer p) ++ " won the game. Congrats.")
         else do
             esp <- readSquare (playerSquareMsg p)
@@ -158,4 +165,72 @@ module Main where
             printBoard nb
             play nb np
 
-    main = play initBoard P1
+    --main = mapM printBoard (aiNonTerminalStates P2)
+    --main = print(length(aiNonTerminalStates P1))
+    --main = print (possibleMoves [[X,X,O],[E,E,E],[E,E,E]])
+
+    randomBoard :: Board
+    randomBoard = [[X,E,O],[E,E,E],[E,E,E]]
+
+    main = print(findValue randomBoard (initSvf P1))
+
+    incMove :: (Int, Int) -> XO -> (Int, Int)
+    incMove (x, o) xo = 
+        if xo == X then (x + 1, o)
+        else if xo == O then (x, o + 1)
+        else (x, o)
+
+    countRowMoves :: Row -> (Int, Int)
+    countRowMoves r = foldl incMove (0, 0) r
+
+    incRowMoves :: (Int, Int) -> Row -> (Int, Int)
+    incRowMoves (x, o) r =
+            (x + x', o + o')
+        where (x', o') = countRowMoves r
+
+    countDoneMoves :: Board -> (Int, Int)
+    countDoneMoves b = foldl (incRowMoves) (0, 0) b
+
+    isPlayerMove :: Board -> Player -> Bool
+    isPlayerMove b p = 
+        if sign p == X then x == o
+        else x == (o + 1)
+        where (x, o) = countDoneMoves b
+
+    rows :: [[XO]]
+    rows = [s| s <- mapM (const [O, X, E]) [0..2]]
+
+    allStates :: States
+    allStates = [b| b <- mapM (const rows) [0..2]]
+
+    aiNonTerminalStates :: Player -> States
+    aiNonTerminalStates ai = filter (\b -> notWin b P1 && notWin b P2 && (isPlayerMove b ai)) allStates
+
+    possibleRowMoves :: Moves -> (Int, Row) -> Moves
+    possibleRowMoves ss (i, r) = (foldl (\ss' -> \t -> if snd t == E then (Square i (fst t)):ss' else ss') ss (zip [0..2] r))
+
+    possibleMoves :: Board -> Moves
+    possibleMoves b = foldl possibleRowMoves [] (zip [0..2] b)
+
+    type StateValue = (Board, Float)
+    type EligibilityTrace = [StateValue]
+    type SVF = [StateValue]
+    type State = Board
+    type Reward = Float
+
+    initStateValues :: Player -> [StateValue]
+    initStateValues p = zip ss (replicate (length ss) 0)
+        where ss = aiNonTerminalStates p
+
+    findValue :: State -> SVF -> Maybe Float
+    findValue b svf = fmap snd (find (\t -> fst t == b) svf)
+
+    eligibility :: EligibilityTrace -> State -> Float -> Maybe Float
+    eligibility et s a = findValue s et 
+
+    evaluate :: SVF -> SVF -> State -> State -> Reward -> SVF
+    evaluate oldSvf svf oldS s r  = 
+        where 
+            d = findValue oldS svf - findValue oldS oldSvf
+            err = r + g + (findValue s svf - findValue oldS svf)
+
